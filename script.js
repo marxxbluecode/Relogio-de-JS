db87cb36d925a39e9a83d75fbf3c52945314f5f9
marxxcodes <marxx171@gmail.com>
2021-05-14 16:07:46 -0300

    Few modifications on main JS and CSS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function carregar(){
     var msg = document.getElementById('box-msg')
-    var titulo = document.querySelector('title')
+    var titulo = document.getElementById('title')
     var img = document.getElementById('pic')
     var data = new Date()
     var hora = data.getHours()
@@ -9,15 +9,15 @@ function carregar(){
     var intervalo = window.setInterval(() => {carregar()},1000);
     msg.innerHTML = `Agora são ${hora}:${minuto}:${segundo}.`
     if(hora >= 6 && hora < 12){
-        // titulo.innerHTML = `Bom Dia!`
+        titulo.innerHTML = 'Bom Dia!'
         img.src = 'images/manha.png'
         document.body.style.background = '#f8d09d'
     }else if(hora >= 12 && hora < 18){
-        // ('Boa Tarde!')
+        titulo.innerHTML = 'Boa Tarde!'
         img.src = 'images/tarde.png'
         document.body.style.background = '#e19981'
     }else {
-        // ('Boa Noite!')
+        titulo.innerHTML = 'Boa Noite!'
         img.src = 'images/noite.png'
         document.body.style.background = '#253745'
     }
